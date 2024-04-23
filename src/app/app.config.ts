50e9a3d53862d2e27a355b9711e8f21e7881f15c
ayyash <abdul_ayyash@yahoo.com>
2024-04-23 10:00:39 +0300

    task01 init

---
+++ src/app/app.config.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig } from '@angular/core';
+
+import { AppRoutingProvider } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: AppRoutingProvider
+};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes, provideRouter } from '@angular/router';
+
+// TASK:01: add routes for bits and pieces. Each route refers to a list component that lists dummy data
+// lazy load the routes
+const routes: Routes = [];
+
+export const AppRoutingProvider = [provideRouter(routes)];
