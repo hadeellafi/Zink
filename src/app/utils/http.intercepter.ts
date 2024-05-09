edaf7b75dce6ac2ce3bfa89f7be6c1e4ecdcc43b
hadeel <hadeellafi9@gmail.com>
2024-05-09 09:23:25 +0300

    task05:log respone in inceptor

--- src/app/utils/http.intercepter.ts
+++ src/app/utils/http.intercepter.ts
@@ -1,5 +1,5 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse } from "@angular/common/http";
-import { map } from "rxjs";
+import { HttpEventType, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse } from "@angular/common/http";
+import { catchError, map, tap, throwError } from "rxjs";
 import { environment } from "../../environments/environment";
 
 const mapData = (res: any) => {
@@ -16,12 +16,32 @@ const mapData = (res: any) => {
 
 // cant use Http in the name so use project name or prefix
 export const DomeInterceptorFn: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+
+
     const url = environment.apiUrl + req.url;
     const adjustedReq = req.clone({ url: url });
 
     // TASK:04: the api returns data in "data" node, map result here.
     return next(adjustedReq).pipe(
-        map(response => mapData(response))
+        map(response => mapData(response)),
+        // add a console logger for all responses, show enough information
+        tap({
+            // don't log errors, just success
+            next: (res) => {
+                // notice the different types of "HttpEvent"
+                if (res.type !== HttpEventType.Sent) {
+                    // TASK:06: refactor out of the interceptor. Create a custom Rxjs operator
+                    // TASK:06: create a console wrapper, move this into Javascript, and declare via typings
+                    console.log(`${req.method} ${req.url} `, res.body);
+                }
+            }
+        }),
+        catchError((err) => {
+            // when loggin an error via catchError, you need to rethrow, or return a new observable
+            console.error('Dome error:', err);
+            // this will log the error again.
+            // TASK:06 stop it from logging its own error using Angular ErrorHandler token
+            return throwError(() => err);
+        })
     );
 };
-
